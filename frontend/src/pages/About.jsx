0291a077ef4237a006571939ab5dfcf83f63f7ef
GreeshKolla123 <2100030262@kluniversity.in>
2025-11-29 00:03:51 +0530

    🚀 Initial commit - Generated with CODE AGENT

---
+++ frontend/src/pages/About.jsx
@@ -0,0 +1,1 @@
+import React from 'react'; function About() { return ( <div> <h1 className="text-3xl">About Us</h1> <p className="text-gray-700">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed sit amet nulla auctor, vestibulum magna sed, convallis ex.</p> </div> ); } export default About;
