aa31049f47b5e1f7f8dee30299687b4f3122ef20
GreeshKolla123 <2100030262@kluniversity.in>
2025-12-01 11:59:18 +0530

    🚀 Initial commit - Generated with CODE AGENT

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,1 +1,1 @@
-import React from 'react'; import ReactDOM from 'react-dom'; import App from './App'; ReactDOM.render(<React.StrictMode><App /></React.StrictMode>, document.getElementById('root'));
+import React from 'react'; import ReactDOM from 'react-dom'; import App from './App'; import { BrowserRouter } from 'react-router-dom'; import { Provider } from 'react-redux'; import store from './store'; ReactDOM.render(<Provider store={store}><BrowserRouter><App /></BrowserRouter></Provider>, document.getElementById('root'));
